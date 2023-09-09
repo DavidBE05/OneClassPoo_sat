9c25f42ab61d6d98f2ce82ad445fb7a30be36f45
DAVID BETANCUR ECHAVARRIA <davidbetancur262776@correo.itm.edu.co>
2023-09-09 11:32:36 -0500

    Class Date cs Created and dateObject buit

---
+++ OneClassPoo_sat/OneClassPoo_sat/Classes/Date.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneClassPoo_sat.Classes
+{
+    internal class Date
+    {
+        #region Fields
+
+        private int _year;
+        private int _month;
+        private int _day;
+        #endregion
+
+        #region Methods
+
+        public Date(int year, int month, int day) // 05/06/2016
+        {
+            _year = year;
+            _month = month; 
+            _day = day;
+        }
+
+        public override string ToString()
+        {
+            var dateConcatenated1 = _day + "/" + _month + "/" + _year;
+            var dateConcatenated2 = $"{_day:00}/{_month:00}/{_year:0000}";
+            var dateConcatenated3 = String.Format("{0:00}/{1:00}/{2:0000}", _day, _month, _year);
+
+            return dateConcatenated3;
+        }
+
+        #endregion
+    }
+}
---
+++ OneClassPoo_sat/OneClassPoo_sat/Program.cs
@@ -0,0 +1,19 @@
+﻿using OneClassPoo_sat.Classes;
+using System;
+
+namespace OneClassPoo_sat
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OOP APLICATTION");
+            Console.WriteLine("-------------");
+
+            //Instaciar las clases Date.cs
+            Date dateObject = new Date(1, 2, 232);
+            Console.WriteLine(dateObject.ToString());
+
+        }
+    }
+}
